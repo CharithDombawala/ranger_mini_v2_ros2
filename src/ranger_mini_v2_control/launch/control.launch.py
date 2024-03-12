5bdfb7220a1dfe5f4a58923345c8f8d63ba9b950
Charitha Dombawala <2000charith@gmail.com>
2024-03-12 13:55:25 +0800

    Update control.launch.py

--- src/ranger_mini_v2_control/launch/control.launch.py
+++ src/ranger_mini_v2_control/launch/control.launch.py
@@ -51,9 +51,9 @@ def generate_launch_description():
             "controller.yaml",
         ]
     )
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("ranger_mini_v2_description"), "rviz", "swerve.rviz"]
-    )
+    #rviz_config_file = PathJoinSubstitution(
+    #    [FindPackageShare("ranger_mini_v2_description"), "rviz", "swerve.rviz"]
+    #)
 
     control_node = Node(
         package="controller_manager",
@@ -61,14 +61,14 @@ def generate_launch_description():
         parameters=[robot_controllers],
         output="both",
     )
-    rviz_node = Node(
-       package="rviz2",
-        executable="rviz2",
-        name="rviz",
-        output="log",
-        arguments=["-d", rviz_config_file],
-        condition=IfCondition(gui),
-    )
+    #rviz_node = Node(
+    #   package="rviz2",
+    #    executable="rviz2",
+    #    name="rviz",
+    #    output="log",
+    #    arguments=["-d", rviz_config_file],
+    #    condition=IfCondition(gui),
+    #)
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -85,12 +85,12 @@ def generate_launch_description():
   
   
     #Delay rviz start after `joint_state_broadcaster`
-    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-           target_action=joint_state_broadcaster_spawner,
-            on_exit=[rviz_node],
-        )
-    )
+    #delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+    #    event_handler=OnProcessExit(
+     #      target_action=joint_state_broadcaster_spawner,
+     #       on_exit=[rviz_node],
+    #    )
+    #)
 
     # Delay start of robot_controller after `joint_state_broadcaster`
     delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
@@ -103,7 +103,7 @@ def generate_launch_description():
     nodes = [
         control_node,
         joint_state_broadcaster_spawner,
-        delay_rviz_after_joint_state_broadcaster_spawner,
+        #delay_rviz_after_joint_state_broadcaster_spawner,
         delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
     ]
 
